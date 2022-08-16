5ad8d0a0c5ab441c43e7a0d8e79d52e3701a6838
Francisco Pugh <franciscopugh01@gmail.com>
2022-08-16 00:43:16 -0300

    Clase 3

---
+++ 37415-react-2022/clase-3/clase-3/src/components/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+
+  let estilos = {
+    backgroundColor : "#000",
+    color: "#fff"
+  }
+
+  return (
+    <div id='' className="" >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+  
+    </div>
+    </div>
+  </nav>
+    </div>
+  );
+}
+
+export default App;
